e2f17a02c124c24d7cad283796b98f4d04af2ebe
veenacc <veena.chenicheri@gmail.com>
2024-07-03 11:51:10 -0700

    added ResumeIndex without Axios

---
+++ src/ResumeIndex.jsx
@@ -0,0 +1,20 @@
+export function ResumeIndex(props) {
+  return (
+    <div>
+      <h1>All Resumes</h1>
+
+      {props.resumes.map((resume) => (
+        <div key={resume.id}>
+           <h2>First Name: {resume.firstName}</h2>
+            <h2>Last Name:{resume.lastName}</h2>
+            <img src={resume.image} width="300" height="300"/>
+            <h2>Job Title:{resume.jobTitle}</h2>
+            <h2>Yrs Of Experience:{resume.experience}</h2>
+            <h2>Skills: {resume.skill}</h2>
+            <hr />
+        </div>
+        ))
+      }
+    </div>
+  );
+}
