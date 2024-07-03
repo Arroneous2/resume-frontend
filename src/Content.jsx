c6e8233d99a04d2eddfaeff345e3f0ff2a302fdc
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-07-03 14:53:48 -0400

    Started show action

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,47 @@
+import { Modal } from "./Modal";
+
 export function Content() {
+
+  const [isResumesShowVisible, setIsResumesShowVisible] = useState(false);
+  const [currentResume, setCurrentResume] = useState({});
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  const handleShowResume = (resume) => {
+    console.log("handleShowResume", resume);
+    setIsResumesShowVisible(true);
+    setCurrentResume(resume);
+  };
+
+  const handleClose = () => {
+    console.log("handleClose");
+    setIsResumesShowVisible(false);
+  };
+
+  
   return (
     <main>
       <h1>Welcome to React!</h1>
+      <ResumesIndex resumes={resumes} onShowResume={handleShowResume} />
+      <Modal show={isResumesShowVisible} onClose={handleClose}>
+        <h1>Test</h1>
+      </Modal>
     </main>
   );
 }
+
+
---
+++ src/ResumeIndex.jsx
@@ -0,0 +1,19 @@
+export function ResumesIndex(props) {
+  return (
+
+
+
+
+
+
+
+
+    <button onClick={() => props.onShowResume(resume)}>More info</button>
+  
+
+
+
+
+
+
+
